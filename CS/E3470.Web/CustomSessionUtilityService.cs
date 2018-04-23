343c8f17be96def47c103731fe10ae48c6f507f8
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/E3470.Web/CustomSessionUtilityService.cs
@@ -0,0 +1,9 @@
+﻿using DevExpress.XtraPrinting;
+using DevExpress.XtraReports.Service.Native.Services;
+
+namespace E3470.Web {
+    public class CustomSessionUtilityService : XRSessionUtilityService {
+        protected override void EnableXpsExport(PrintingSystemBase printingSystem) {
+        }
+    }
+}
---
+++ VB/E3470.Web/CustomSessionUtilityService.vb
@@ -0,0 +1,12 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports DevExpress.XtraPrinting
+Imports DevExpress.XtraReports.Service.Native.Services
+
+Namespace E3470.Web
+	Public Class CustomSessionUtilityService
+		Inherits XRSessionUtilityService
+		Protected Overrides Sub EnableXpsExport(ByVal printingSystem As PrintingSystemBase)
+		End Sub
+	End Class
+End Namespace

54f63b9a5763ad5abe6bc8554c19ec6d5839612b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/E3470.Web/CustomPrintingSystemFactory.vb
@@ -0,0 +1,16 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports DevExpress.Xpf.Printing.Service.Native.Services.Factories
+Imports DevExpress.XtraPrinting
+
+Namespace E3470.Web
+	Public Class CustomPrintingSystemFactory
+		Inherits PrintingSystemFactory
+		Public Sub New(ByVal exportFactory As IExportFactory)
+			MyBase.New(exportFactory)
+		End Sub
+
+		Public Overrides Sub EnableXpsExport(ByVal printingSystem As PrintingSystemBase)
+		End Sub
+	End Class
+End Namespace
